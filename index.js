3bb2f834165ec3da903055863cd930600694afea
vzaidman <vzaidman@gmail.com>
2017-04-16 15:50:00 +0300

    changed the way the index file is served

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use('/', express.static(__dirname + '/public'))
+app.use('/images', express.static(__dirname + '/public/images'))
+
+app.get('*', function(req, res){
+  res.sendfile('index.html', {root: __dirname + '/public'});
+});
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'))
