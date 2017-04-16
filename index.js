06d24e767a5b912b8e0e4e9ea4d7cfd4a5f62ec5
vzaidman <vzaidman@gmail.com>
2017-04-16 14:58:43 +0300

    initial working reembeding

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'))
