07fea625333e3b399c3dbfd0babb11964148d275
vzaidman <vzaidman@gmail.com>
2017-04-16 16:12:16 +0300

    added favicon

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ app.set('port', (process.env.PORT || 5000))
 
 app.use('/images', express.static(__dirname + '/public/images'))
 
+app.get('/favicon.ico', function(req, res){
+  res.sendfile('favicon.ico', {root: __dirname});
+})
+
 app.get('*', function(req, res){
   res.sendfile('index.html', {root: __dirname + '/public'});
-});
+})
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'))
