bec8db588be70c6e74a7ff3ebe536ba0b7676d0a
vzaidman <vzaidman@gmail.com>
2017-04-16 15:16:26 +0300

    improved links

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'))
