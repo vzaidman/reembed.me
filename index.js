4032f7ffe7a43e2603d00db3c873149aaae1c5d6
vzaidman <vzaidman@gmail.com>
2017-04-17 12:35:52 +0300

    made client directory static

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use('/images', express.static(__dirname + '/public/images'))
-
 app.get('/favicon.ico', function(req, res){
-  res.sendFile('favicon.ico', {root: __dirname + '/public'});
+  res.sendFile('favicon.ico', {root: __dirname + '/public'})
 })
 
 app.get('/redirect/*', function(req, res){
-  res.sendFile('redirect.html', {root: __dirname + '/public'});
+  res.sendFile('redirect.html', {root: __dirname + '/public'})
 })
 
-app.get('*', function(req, res){
-  res.sendFile('index.html', {root: __dirname + '/public/client'});
-})
+app.use('/images', express.static(__dirname + '/public/images'))
+
+app.use('*', express.static(__dirname + '/public/client'))
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'))
